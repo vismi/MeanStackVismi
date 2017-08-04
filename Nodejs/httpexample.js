b2d00660bbfec97c84883d5c84a46c9465148c62
vismi <vismi.25@gmail.com>
2017-08-04 20:33:23 +0530

    completed simple HTML + CSS page

--- Nodejs/httpexample.js
+++
@@ -1,6 +0,0 @@
-	var http = require('http');
-	var servervariable = http.createServer(function(reqest,response){
-		response.writeHead(200);
-		response.end('Hello Http : after writeHead');
-	});
-	servervariable.listen(8080);
